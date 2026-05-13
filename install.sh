bd4b5495b587d5ccbb76bf9c4347e5a737d6c3f2
weizman <weizmangal@gmail.com>
2026-05-13 15:40:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ echo "Installing 'rand' command to $TARGET..."
 
 cat > "$TARGET" << 'EOF'
 #!/usr/bin/env bash
-echo $(( (RANDOM % 1000) + 1 ))
+echo $(( (RANDOM % 100000) + 1 ))
 EOF
 
 chmod +x "$TARGET"
 
-echo "Done! Run 'rand' to get a random number between 1 and 1000."
+echo "Done! Run 'rand' to get a random number between 1 and 100000."
